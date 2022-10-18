3cd34552b69cb8bd5b8850137792a8b0c15002b0
Diaz <carlosd2mil@gmail.com>
2022-10-17 21:52:32 -0500

    It's alive!

--- Modulo4/Actividad4.1/main.cpp
+++ Modulo4/Actividad4.1/main.cpp
@@ -1,4 +1,7 @@
-
+// Program checks if two lines intersect each other
+// Carlos Daniel Diaz Arrazate - A01734902
+// Jose Angel Gonzalez Carrera - A01552274
+// Carlos Eduardo Ruiz Lira - A01735706
 #include <iostream>
 #include <string>
 
@@ -12,12 +15,12 @@ struct Point {
         x = pX;
         y = pY;
     }
-
-    void compare(int p2, int q2) {
-        // comparation
-    }
 };
 
+// Calculate orientation
+// Input: Point, Point, Point
+// Output: Orientation
+// Time complexity: O(1)
 string orientation(Point p, Point q, Point r) {
     int val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
 
@@ -33,6 +36,10 @@ string orientation(Point p, Point q, Point r) {
 }
 
 
+// Check if lines intersect each other
+// Input: Point of line 1, Point of line 1, Point of line 2, Point of line 2
+// Output: Bool
+// Time complexity: O(1)
 bool intersect(Point p1, Point q1, Point p2, Point q2) {
     
     string o1 = orientation(p1, q1, p2);
@@ -48,48 +55,50 @@ bool intersect(Point p1, Point q1, Point p2, Point q2) {
     }
 }
 
+
+// Receive coordinates and check if lines intersect
+// Input: Coordinates of extremes of 2 lines
+// Output: Bool
+// Time complexity: O(1)
 bool getIntersection(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
     Point p1 = Point(x1,y1);
     Point q1 = Point(x2,y2);
     Point p2 = Point(x3,y3);
     Point q2 = Point(x4,y4);
+    /*
     cout << "P1: " << p1.x << " " << p1.y << endl;
     cout << "Q1: " << q1.x << " " << q1.y << endl;
     cout << "P2: " << p2.x << " " << p2.y << endl;
     cout << "Q2: " << q2.x << " " << q2.y << endl;
+    */
     return intersect(p1, q1, p2, q2);
 }
 
 int main() {
-    //Point p1 = { 0, 0 }, q1 = { 4, 4 };
-    //Point p2 = { 1, 2 }, q2 = { 10, 2};
-    //Point p1 = { 0, 6 }, q1 = { 6, 0 };
-    //Point p2 = { 0, 0 }, q2 = { 5, 5};
-
     int n;
     cin >> n;
 
     int point[8];
     bool result[n];
 
-    cout << "---Cases---" << endl;
+    // cout << "---Cases---" << endl;
     for(int i=0; i<n; i++) {
-        cout << "Case " << i << endl;
+        // cout << "Case " << i << endl;
         for(int j=0; j<8; j++) {
             cin >> point[j];
         }
         result[i] = getIntersection(point[0], point[1], point[2], point[3], point[4], point[5], point[6], point[7]);
-        cout << endl;
+        // cout << endl;
     }
 
-    cout << endl;
+    // cout << endl;
 
 
-    cout << boolalpha << "---RESULTS---" << endl;
+    // cout << boolalpha << "---RESULTS---" << endl;
+    cout << boolalpha;
     for(int i=0; i<n; i++) {
         cout << result[i] << endl;
     }
-    cout << endl;
 
 
     return 0;
